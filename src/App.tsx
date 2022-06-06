ee345ff40a599d794bf57141c070c7042e372c4e
Your Name01~ <you@example.com>
2022-06-05 22:04:41 -0300

    init website

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Icon, Nav, Navbar, NavDropdown } from "dressa-ui";
+
+function App() {
+  return (
+    <>
+      <Navbar>
+        <Navbar.Brand children='DressaUI' />
+        <>Um design system clean ;)</>
+        <Navbar.Toggle aria-controls="basic" />
+        <Navbar.Collapse id="basic">
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href='/components'>Components</Nav.Link>
+            <NavDropdown title="Dev" id="dropdown-1">
+              <NavDropdown.Item href="https://github.com/drelocatelli/dressa-ui-package">Github</NavDropdown.Item>
+              <NavDropdown.Item href="https://www.npmjs.com/package/dressa-ui">NPM</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+export default App;
